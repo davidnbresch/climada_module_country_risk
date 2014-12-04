a62e51c2782bf5ee75ef0a897b75db278d2506ed
davidnbresch <david.bresch@gmail.com>
2014-12-04 09:14:42 +0100

    cleanup

--- code/climada_high_res_entity.m
+++ code/climada_high_res_entity.m
@@ -4,13 +4,13 @@ function entity=climada_high_res_entity(img_filename,check_plot,select_admin0,ad
 %	climada_high_res_entity
 % PURPOSE:
 %   Construct an entity file baed on high-res night light data. Reads an
-%   image file with nightlight density and matches it to the local geography 
-%   
+%   image file with nightlight density and matches it to the local geography
+%
 %   Prompts for country (admin0) and state/province (admin1), fetches the
 %   tile of night light density from www, constrains the active centroids
 %   (with values>0) to the selected country or admin1 (see
-%   restrict_Values_to_admin in PARAMETERS in code) and saves the entity. 
-%   
+%   restrict_Values_to_admin in PARAMETERS in code) and saves the entity.
+%
 %   Since we're dealing with admin1, no automatic scaling or allocation of
 %   GDP to centroids is performed (see climada_create_GDP_entity)
 %
@@ -55,12 +55,16 @@ function entity=climada_high_res_entity(img_filename,check_plot,select_admin0,ad
 %       Most useful for subsequent calls, i.e. once one knows the exact
 %       admin1 name. Also useful if a img_filename is passed and thus if
 %       admin1_name is defined, the respective admin1 is cut out.
+%       NOTE: Still an issue with ?..., i.e. Zrich does not work if entered
+%       as admin1_name, but works if selected from list-dialog
 % OUTPUTS:
-%   plots
+%   entity: a full climada entity, see climada_entity_read
+%       and see e.g. climada_entity_plot to check
 % RESTRICTIONS:
 % MODIFICATION HISTORY:
 % David N. Bresch, david.bresch@gmail.com, 20141202
 % David N. Bresch, david.bresch@gmail.com, 20141203, country and admin1 selection
+% David N. Bresch, david.bresch@gmail.com, 20141204, 'ASK' debugged
 %-
 
 entity=[]; % init
@@ -111,14 +115,14 @@ entity_file=[GDP_entity_data_folder filesep 'entity_global_without_assets.xls'];
 close all % for TEST
 
 % read admin0 (country) shape file (we need this in any case)
-admin0_shapes=shaperead(admin0_shape_file); 
+admin0_shapes=shaperead(admin0_shape_file);
 admin1_shapes=[]; % init
 
 selection_admin0_shape_i=[]; % init
 selection_admin1_shape_i=[]; % init
-    
+
 if isempty(img_filename) % local GUI
-        
+    
     % no filename passed, we ask for admin0 and admin1...
     
     if isempty(admin0_name)
@@ -139,7 +143,7 @@ if isempty(img_filename) % local GUI
         else
             img_filename='ASK'; % Cancel pressed, later prompt for filename
         end
- 
+        
     end % isempty(admin0_name)
     
     % find the country in the shape file
@@ -152,95 +156,99 @@ if isempty(img_filename) % local GUI
     
     selection_admin0_shape_i=admin0_shape_i;
     
-    if select_admin0
+    if ~strcmp(img_filename,'ASK')
         
-        % prepare parameters for www call to fetch the tile of the global map
-        bbox(1)=floor(admin0_shapes(selection_admin0_shape_i).BoundingBox(1));
-        bbox(3)=ceil(admin0_shapes(selection_admin0_shape_i).BoundingBox(2));
-        bbox(2)=floor(admin0_shapes(selection_admin0_shape_i).BoundingBox(3));
-        bbox(4)=ceil(admin0_shapes(selection_admin0_shape_i).BoundingBox(4));
-        admin1_name='';
-        
-    else
+        if select_admin0
             
-        % add (country states/provinces)
-        fprintf('processing admin1 shapes ...\n'); % prompt, since takes a bit of time...
-        if isempty(admin1_shapes),admin1_shapes=shaperead(admin1_shape_file);end % read admin1 shape file
-        % figure which shapes within the country we need
-        admin1_shape_i=0;next_admin1=1; % init
-        for shape_i=1:length(admin1_shapes)
-            for country_i=1:length(admin0_shape_i)
-                if strcmp(admin0_shapes(admin0_shape_i(country_i)).ADMIN,admin1_shapes(shape_i).admin)
-                    admin1_shape_i(next_admin1)=shape_i;
-                    next_admin1=next_admin1+1;
-                end
-            end % country_i
-        end % shape_i
-        
-        if isempty(admin1_name)
-            
-            % plot admin0 (country) shape(s)
-            for admin0_i=1:length(admin0_shape_i)
-                shape_i=admin0_shape_i(admin0_i);
-                plot(admin0_shapes(shape_i).X,admin0_shapes(shape_i).Y,'-r','LineWidth',2);
-                hold on; axis equal
-            end % country_i
-            set(gcf,'Color',[1 1 1]) % whithe figure background
-            
-            % plot admin1 (country states/provinces) shapes
-            liststr=[]; % reset
-            for admin1_i=1:length(admin1_shape_i)
-                shape_i=admin1_shape_i(admin1_i);
-                plot(admin1_shapes(shape_i).X,admin1_shapes(shape_i).Y,'-r','LineWidth',1);
-                text(admin1_shapes(shape_i).longitude,admin1_shapes(shape_i).latitude,admin1_shapes(shape_i).name);
-                liststr{admin1_i}=admin1_shapes(shape_i).name; % compile list of admin1 names
-            end % admin1_i
-            
-            % show list dialog to select admin1 (now easy as names shown on plot)
-            [selection,ok] = listdlg('PromptString','Select admin1:',...
-                'ListString',liststr,'SelectionMode','single');
-            if ~ok,return;end
-            pause(0.1)
-            if ~isempty(selection)
-                admin1_name=['_' deblank(liststr{selection})]; % _ for filename, see below
-                selection_admin1_shape_i=admin1_shape_i(selection);
-            else
-                return
-            end % ~isempty(selection)
+            % prepare parameters for www call to fetch the tile of the global map
+            bbox(1)=floor(admin0_shapes(selection_admin0_shape_i).BoundingBox(1));
+            bbox(3)=ceil(admin0_shapes(selection_admin0_shape_i).BoundingBox(2));
+            bbox(2)=floor(admin0_shapes(selection_admin0_shape_i).BoundingBox(3));
+            bbox(4)=ceil(admin0_shapes(selection_admin0_shape_i).BoundingBox(4));
+            admin1_name='';
             
         else
-                                    
+            
+            % add (country states/provinces)
+            fprintf('processing admin1 shapes ...\n'); % prompt, since takes a bit of time...
+            if isempty(admin1_shapes),admin1_shapes=shaperead(admin1_shape_file);end % read admin1 shape file
+            % figure which shapes within the country we need
+            admin1_shape_i=0;next_admin1=1; % init
             for shape_i=1:length(admin1_shapes)
-                if strcmp(admin1_shapes(shape_i).name,admin1_name)
-                    selection_admin1_shape_i=shape_i;
-                end
+                for country_i=1:length(admin0_shape_i)
+                    if strcmp(admin0_shapes(admin0_shape_i(country_i)).ADMIN,admin1_shapes(shape_i).admin)
+                        admin1_shape_i(next_admin1)=shape_i;
+                        next_admin1=next_admin1+1;
+                    end
+                end % country_i
             end % shape_i
             
-        end % isempty(admin1_name)
+            if isempty(admin1_name)
                 
+                % plot admin0 (country) shape(s)
+                for admin0_i=1:length(admin0_shape_i)
+                    shape_i=admin0_shape_i(admin0_i);
+                    plot(admin0_shapes(shape_i).X,admin0_shapes(shape_i).Y,'-r','LineWidth',2);
+                    hold on; axis equal
+                end % country_i
+                set(gcf,'Color',[1 1 1]) % whithe figure background
+                
+                % plot admin1 (country states/provinces) shapes
+                liststr=[]; % reset
+                for admin1_i=1:length(admin1_shape_i)
+                    shape_i=admin1_shape_i(admin1_i);
+                    plot(admin1_shapes(shape_i).X,admin1_shapes(shape_i).Y,'-r','LineWidth',1);
+                    text(admin1_shapes(shape_i).longitude,admin1_shapes(shape_i).latitude,admin1_shapes(shape_i).name);
+                    liststr{admin1_i}=admin1_shapes(shape_i).name; % compile list of admin1 names
+                end % admin1_i
+                
+                % show list dialog to select admin1 (now easy as names shown on plot)
+                [selection,ok] = listdlg('PromptString','Select admin1:',...
+                    'ListString',liststr,'SelectionMode','single');
+                if ~ok,return;end
+                pause(0.1)
+                if ~isempty(selection)
+                    admin1_name=['_' deblank(liststr{selection})]; % _ for filename, see below
+                    selection_admin1_shape_i=admin1_shape_i(selection);                    
+                else
+                    return
+                end % ~isempty(selection)
+                
+            else
+                
+                for shape_i=1:length(admin1_shapes)
+                    if strcmp(admin1_shapes(shape_i).name,admin1_name)
+                        selection_admin1_shape_i=shape_i;
+                    end
+                end % shape_i
+                
+            end % isempty(admin1_name)
+            
+            % prepare parameters for www call to fetch the tile of the global map
+            bbox(1)=floor(admin1_shapes(selection_admin1_shape_i).BoundingBox(1));
+            bbox(3)=ceil(admin1_shapes(selection_admin1_shape_i).BoundingBox(2));
+            bbox(2)=floor(admin1_shapes(selection_admin1_shape_i).BoundingBox(3));
+            bbox(4)=ceil(admin1_shapes(selection_admin1_shape_i).BoundingBox(4));
+            
+        end % select_admin0
+        
+        % make sure at least 2 deg in each direction
+        if abs(bbox(3)-bbox(1))<2,bbox(1)=bbox(1)-1;bbox(3)=bbox(3)+1;end
+        if abs(bbox(4)-bbox(2))<2,bbox(2)=bbox(2)-1;bbox(4)=bbox(4)+1;end
         % prepare parameters for www call to fetch the tile of the global map
-        bbox(1)=floor(admin1_shapes(selection_admin1_shape_i).BoundingBox(1));
-        bbox(3)=ceil(admin1_shapes(selection_admin1_shape_i).BoundingBox(2));
-        bbox(2)=floor(admin1_shapes(selection_admin1_shape_i).BoundingBox(3));
-        bbox(4)=ceil(admin1_shapes(selection_admin1_shape_i).BoundingBox(4));
+        bbox_file_pref=sprintf('%i_%i_%i_%i_',bbox);
         
-    end % select_admin0
-    
-    % make sure at least 2 deg in each direction
-    if abs(bbox(3)-bbox(1))<2,bbox(1)=bbox(1)-1;bbox(3)=bbox(3)+1;end
-    if abs(bbox(4)-bbox(2))<2,bbox(2)=bbox(2)-1;bbox(4)=bbox(4)+1;end
-    % prepare parameters for www call to fetch the tile of the global map
-    bbox_file_pref=sprintf('%i_%i_%i_%i_',bbox);
-    
-    if isempty(img_filename)
+        if isempty(img_filename)
+            
+            % construct the filename and fetch the image tile from www
+            
+            img_filename=[climada_global.data_dir filesep 'results' filesep bbox_file_pref admin0_name admin1_name '_high_res.avg_lights.lzw.tiff'];
+            fprintf('%s\n',img_filename);
+            
+            if ~fetch_mapserver_ngdc_noaa_gov_gcv4(bbox,img_filename),return;end
+        end
         
-        % construct the filename and fetch the image tile from www
-        
-        img_filename=[climada_global.data_dir filesep 'results' filesep bbox_file_pref admin0_name admin1_name '_high_res.avg_lights.lzw.tiff'];
-        fprintf('%s\n',img_filename);
-        
-        if ~fetch_mapserver_ngdc_noaa_gov_gcv4(bbox,img_filename),return;end
-    end
+    end % ~strcmp(img_filename,'ASK')
     
 end % isempty(img_filename)
 
